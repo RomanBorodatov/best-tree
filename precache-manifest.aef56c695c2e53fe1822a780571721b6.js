7db1ec6045e8950da2df5f0a44e09f067e739b27
Roman Borodatov <roman.borodatov@vitd.co>
2019-06-30 20:34:23 +0300

    Updates

---
+++ precache-manifest.aef56c695c2e53fe1822a780571721b6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/best-tree/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fc047347b17f7df7ff288d78c8c281cf",
+    "url": "/best-tree/static/media/avatar.fc047347.png"
+  },
+  {
+    "revision": "f199bbcc47460e067866",
+    "url": "/best-tree/static/js/runtime~main.f199bbcc.js"
+  },
+  {
+    "revision": "3a1d24e1fa962c8c9c29",
+    "url": "/best-tree/static/js/main.3a1d24e1.chunk.js"
+  },
+  {
+    "revision": "fd3d8473deb5077d4698",
+    "url": "/best-tree/static/js/1.fd3d8473.chunk.js"
+  },
+  {
+    "revision": "3a1d24e1fa962c8c9c29",
+    "url": "/best-tree/static/css/main.a5abd74c.chunk.css"
+  },
+  {
+    "revision": "df2b9ca601a89576828745bcd760b461",
+    "url": "/best-tree/index.html"
+  }
+];
